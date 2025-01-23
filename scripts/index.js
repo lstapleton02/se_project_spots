dd4b7ff2b43daffefef87fe8f79aead263709b4a
Lindsay Stapleton <lindsay.stapleton02@gmail.com>
2025-01-23 12:46:59 -0500

    feat: add modal preview and update styles

--- scripts/index.js
+++ scripts/index.js
@@ -31,7 +31,7 @@ const editModalButton = document.querySelector(".profile__edit-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
-// Form Elements
+// Modal Form Elements
 const editModal = document.querySelector("#edit-modal");
 const editFormElement = editModal.querySelector(".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
@@ -42,12 +42,23 @@ const editModalDescriptionInput = editModal.querySelector(
 
 //Card Modal Elements
 const cardModal = document.querySelector("#add-card-modal");
+const cardForm = cardModal.querySelector(".modal__form");
 const cardModalBtn = document.querySelector(".profile__add-btn");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
+const cardNameInput = cardModal.querySelector("#add-card-name-input");
+const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 
 //Card related elements
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
+
+//Preview modal
+const previewModal = document.querySelector("#preview-modal");
+const previewModalImgEl = previewModal.querySelector(".modal__image");
+const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
+const previewModalCloseBtn = previewModal.querySelector(
+  ".modal__close_type_preview"
+);
 
 function getCardElement(data) {
   const cardElement = cardTemplate.content
@@ -55,10 +66,30 @@ function getCardElement(data) {
     .cloneNode(true);
 
   const cardNameEl = cardElement.querySelector(".card__title");
-  cardNameEl.textContent = data.name;
   const cardImage = cardElement.querySelector(".card__image");
+  const cardLikeBtn = cardElement.querySelector(".card__like-btn");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-btn");
+
+  cardNameEl.textContent = data.name;
   cardImage.src = data.link;
   cardImage.alt = data.name;
+
+  cardLikeBtn.addEventListener("click", () => {
+    cardLikeBtn.classList.toggle("card__like-btn_liked");
+  });
+
+  cardDeleteBtn.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  //event handler open and close the preview mode of each image
+  cardImage.addEventListener("click", () => {
+    openModal(previewModal);
+
+    previewModalImgEl.src = cardImage.src;
+    previewModalImgEl.alt = cardImage.name;
+    previewModalCaptionEl.textContent = cardNameEl.textContent;
+  });
 
   return cardElement;
 }
@@ -84,6 +115,7 @@ editModalCloseBtn.addEventListener("click", () => {
   closeModal(editModal);
 });
 
+//profile modal submit
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
 
@@ -93,8 +125,21 @@ function handleEditFormSubmit(evt) {
   closeModal(editModal);
 }
 
+//card modal submit
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
+  const cardElement = getCardElement(inputValues);
+  cardsList.prepend(cardElement);
+
+  closeModal(cardModal);
+}
+
 // Connect the handler to the form, so it will watch for the submit event.
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+//submit handler for adding new post
+cardForm.addEventListener("submit", handleAddCardSubmit);
 
 //Open and Close the New Post - Card Modal
 cardModalBtn.addEventListener("click", () => {
@@ -103,6 +148,10 @@ cardModalBtn.addEventListener("click", () => {
 //profileEditButton.addEventListener("click", openModal);
 cardModalCloseBtn.addEventListener("click", () => {
   closeModal(cardModal);
+});
+
+previewModalCloseBtn.addEventListener("click", () => {
+  closeModal(previewModal);
 });
 
 //Initial Posts
